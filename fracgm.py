3bd858836171dc8e22ea8749ddcfd236a67a487e
Bang-Shien Chen <dgbshien@gmail.com>
2024-09-17 04:59:27 +0000

    :dog: update output form

--- fracgm.py
+++ fracgm.py
@@ -3,15 +3,14 @@ from scipy.optimize import minimize_scalar
 
 class FracGM:
     """ 
-    FracGM solver for the optimization problem
-    min x^2 / (x^2 + 1),
+    FracGM solver for the optimization problem:
+        min x^2 / (x^2 + 1),
     where the optimal solution is x* = 0.
     """
     def __init__(self, max_iteration=10, tolerance=1e-7, verbose=False):
         
         self.max_iter = max_iteration
         self.tol = tolerance
-
         self.verbose = verbose
 
     @staticmethod
@@ -23,24 +22,27 @@ class FracGM:
         return x**2 + 1
 
     def solve_beta_mu(self, x):
-        "Solve beta & mu by a linear system."
+        
         beta = self.f(x)/self.h(x)
         mu = 1/self.h(x)
+        
         return beta, mu
 
     def solve_x(self, beta, mu):
-        "Solve dual problem by Scipy minimizer for scalar function of one variable.."
+        
         def dual_problem(x):
             return mu*(x**2 - beta*(x**2 + 1)) 
-
+        
+        # Scipy minimizer for scalar function of one variable
         res = minimize_scalar(dual_problem)
-
+        
         return res.x
 
     def compute_psi_norm(self, beta, mu, x):
-        "Compute the norm of psi for stopping criteria."
+        
         psi_1 = -self.f(x) + beta*self.h(x)
         psi_2 = -1 + mu*self.h(x)
+        
         return np.linalg.norm([psi_1, psi_2])
 
     def solve(self, initial):
@@ -56,11 +58,12 @@ class FracGM:
             else:
                 x = self.solve_x(beta, mu)
                 beta, mu = self.solve_beta_mu(x)
-                # stopping criteria
+
                 norm = self.compute_psi_norm(beta, mu, x)
                 if norm < self.tol:
                     break
 
         if self.verbose:
-            print("solution:", x)
+            print("solution:", '{:.4f}'.format(x))
+            
         return x
