8761ffde82a1372da128e12f3ab4b32bf08b6f3c
Bang-Shien Chen <dgbshien@gmail.com>
2024-09-17 05:02:39 +0000

    :dog: adjust output form

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fracgm import FracGM
-model = FracGM(verbose=True)
 
 # test variaous initial values
 initial_list = [-10000, -1000, -100, -10, -1, 0, 1, 10, 100, 1000, 10000]
 
 for initial_guess in initial_list:
-    print("="*50)
-    model.solve(initial=initial_guess)
+    print("="*20)
+    FracGM().solve(initial=initial_guess)
